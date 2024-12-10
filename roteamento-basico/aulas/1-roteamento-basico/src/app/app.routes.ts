a300d4a1d47dbbc7156a4e3d0fdba0092ae29095
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-10 15:24:33 -0300

    lazy loading

--- roteamento-basico/aulas/1-roteamento-basico/src/app/app.routes.ts
+++ roteamento-basico/aulas/1-roteamento-basico/src/app/app.routes.ts
@@ -4,10 +4,25 @@ import { SegundoComponent } from './components/segundo/segundo.component';
 import { BaseComponent } from './components/base/base.component';
 
 export const routes: Routes = [
-    { path: "componentes", component: BaseComponent },
-    { path: "componentes/primeiro", component: PrimeiroComponent },
-    { path: "componentes/segundo", component: SegundoComponent }
+    { 
+        path: "componentes",
+        loadComponent: () => import('./components/base/base.component').then((m) => m.BaseComponent)
+    },
+    { 
+        path: "componentes/primeiro",
+        loadComponent: () => import('./components/primeiro/primeiro.component').then((m) => m.PrimeiroComponent)
+    },
+    { 
+        path: "componentes/segundo",
+        loadComponent: () => import('./components/segundo/segundo.component').then((m) => m.SegundoComponent)
+    }
 ];
+
+// export const routes: Routes = [
+//     { path: "componentes", component: BaseComponent },
+//     { path: "componentes/primeiro", component: PrimeiroComponent },
+//     { path: "componentes/segundo", component: SegundoComponent }
+// ];
 
 // export const routes: Routes = [
 //     { path: "primeiro", component: PrimeiroComponent },
