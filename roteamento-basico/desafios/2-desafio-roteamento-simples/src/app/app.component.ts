19f28c212273a04439420f884bf62f373fc50ca5
filipe-fff <pintovicentinifilipe@gmail.com>
2024-12-16 23:24:57 -0300

    basico dos componentes feito

--- roteamento-basico/desafios/2-desafio-roteamento-simples/src/app/app.component.ts
+++ roteamento-basico/desafios/2-desafio-roteamento-simples/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ MatIcon, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
